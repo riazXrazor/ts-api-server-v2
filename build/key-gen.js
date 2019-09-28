0ccf6e163963a1cc89428522a45743860880c3a9
Riaz Laskar <riazcool77@gmail.com>
2019-09-28 18:57:35 +0530

    update

--- build/key-gen.js
+++ build/key-gen.js
@@ -5,8 +5,8 @@ const fs = require("fs");
 
 const pair = keypair();
 const start = process.hrtime();
-fs.writeFileSync("build/private.key",pair.private);
-fs.writeFileSync("build/public.key",pair.public);
+fs.writeFileSync("src/certs/private.key",pair.private);
+fs.writeFileSync("src/certs/public.key",pair.public);
 
 const end = process.hrtime(start);
 console.log(chalk.greenBright(`✓ key files generated (${prettyHrtime(end)})`));
